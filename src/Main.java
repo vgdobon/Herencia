861a964ea451abeb2f91c0f8c06bd2f3162db065
vgdobon <vgdobon91@gmail.com>
2020-09-02 12:52:45 +0200

    Herencia con java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Matematicas mates = new Matematicas();
+
+       mates.sum(5,3);
+
+    }
+}
---
+++ src/Matematicas.java
@@ -0,0 +1,10 @@
+public class Matematicas extends Logger {
+
+    public void sum(int num1, int num2){
+
+        int resultado = num1 + num2;
+        log(String.valueOf(resultado));
+    }
+
+
+}
